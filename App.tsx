3f5a7f583db6e6d1dad24ed031cbee4324459bf6
Matthew Macartney <matthew.macartney@codecomputerlove.com>
2021-08-05 17:42:49 +0100

    added react navigation

--- App.tsx
+++ App.tsx
@@ -1,115 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import * as React from 'react';
+import { View, Text, TouchableHighlight } from 'react-native';
+import { NavigationContainer, RouteProp } from '@react-navigation/native';
+import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
 
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
+type RootStackParamList = {
+    Home: undefined,
+    About: undefined
+}
 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
+type ProfileScreenRouteProp = RouteProp<RootStackParamList, 'Home'>;
+type ProfileScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
 
- const Section: React.FC<{
-   title: string;
- }> = ({children, title}) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
+type ScreenProps = {
+    route: ProfileScreenRouteProp,
+    navigation: ProfileScreenNavigationProp
+}
 
- const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
+function HomeScreen({ route, navigation } : ScreenProps) {
 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
+    const navigate = () => {
+        navigation.navigate('About');
+    }
 
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>{route.name} Screen</Text>
+            <TouchableHighlight onPress={() => navigate() }>
+                <Text>Go to Not Home Screen</Text>
+            </TouchableHighlight>
+        </View>
+    );
+}
 
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
+function AboutScreen({ route, navigation } : ScreenProps) {
 
- export default App;
+    const navigate = () => {
+        navigation.navigate('Home');
+    }
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>{route.name} Screen</Text>
+            <TouchableHighlight onPress={() => navigate() }>
+                <Text>Go to Home Screen</Text>
+            </TouchableHighlight>
+        </View>
+    );
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName={'Home'}>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="About" component={AboutScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
